e600c8d062012f01b29d6018c474900cc8f14b6d
Denis Kozyrev <deniskozyrev55@gmail.com>
2019-12-11 16:54:54 +0300

    added sequelize package and implemented sequelize config

---
+++ src/models/modelsWithUsingNativeSql/product.js
@@ -0,0 +1,35 @@
+// working with data with database(SQL)
+const { deleteProductFromCart } = require("./cart");
+
+const dataBase = require("../utils/mySqlNativeConfig/sqlDatabase");
+
+module.exports = class Product {
+  constructor(id, title, price, description, imageUrl) {
+    this.id = id;
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
+
+  addProduct() {
+    return dataBase.execute(
+      "INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)",
+      [this.title, this.price, this.description, this.imageUrl]
+    );
+  }
+
+  static editProduct() {}
+
+  static deleteProduct(id) {}
+
+  static fetchAllProducts() {
+    return dataBase.execute("SELECT * FROM products");
+  }
+
+  static findProductById(id) {
+    return dataBase.execute("SELECT * FROM products WHERE products.id = ?", [
+      id
+    ]);
+  }
+};
--- src/utils/sqlDatabase.js
+++ src/utils/sqlDatabase.js
@@ -1,12 +1,8 @@
-// get the client
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-// Create the connection pool. The pool-specific settings are the defaults
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "node_complete",
-  password: "nodecomplete"
+const sequelize = new Sequelize("node_comlete", "root", "nodecomplete", {
+  dialect: "mysql",
+  host: "localhost"
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;

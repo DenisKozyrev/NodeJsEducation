b05eb0743ab5120c5cdaca7a2d377bfa458cce7a
Denis Kozyrev <deniskozyrev55@gmail.com>
2019-12-13 17:07:07 +0300

    improved cart creation in app root file

--- app.js
+++ app.js
@@ -20,30 +20,29 @@ app.set("view engine", "ejs"); // set the template engine
 app.set("views", "src/views"); // path to views file with pug or over extensions.
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static(path.join(rootDir, "public")));
 
 app.use((req, res, next) => {
   User.findOne({ where: { id: 1 } })
     .then(user => {
+      console.log(user);
       req.user = user;
       next();
     })
     .catch(err => console.log(err));
 });
-
 app.use("/admin", adminRouter); //filtering and adding same path beging
 app.use(shopRouter);
-
 app.use(errorsController.getNotFoundPage);
 
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-// Cart.belongsTo(User); //optional, we can set only one relation User.hasOne(Cart);
+// Relations
+User.hasMany(Product, { constraints: true, onDelete: "CASCADE" });
 User.hasOne(Cart);
+// Cart.belongsTo(User); //optional, we can set only one relation User.hasOne(Cart);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
+// DataBase Sync
 nodeCompleteDB
   // .sync({ force: true })
   .sync()
@@ -57,7 +56,12 @@ nodeCompleteDB
     return user;
   })
   .then(user => {
-    return user.createCart();
+    Cart.findOne({ where: { id: 1 } }).then(cart => {
+      if (!cart) {
+        return user.createCart();
+      }
+      return cart;
+    });
   })
   .then(() => {
     app.listen(3000);
